4ab6211651bfc0b98fdec3d16713cc097f45477f
rp <arpeedesign@gmail.com>
2023-03-31 11:16:52 +0200

    shop with products list

--- src/main/java/com/portfolio/webshop_0321/controller/ShoppingCartController.java
+++ src/main/java/com/portfolio/webshop_0321/controller/ShoppingCartController.java
@@ -2,7 +2,9 @@ package com.portfolio.webshop_0321.controller;
 
 import com.portfolio.webshop_0321.entity.CartItem;
 import com.portfolio.webshop_0321.entity.Customer;
+import com.portfolio.webshop_0321.entity.Product;
 import com.portfolio.webshop_0321.service.CustomerService;
+import com.portfolio.webshop_0321.service.ProductService;
 import com.portfolio.webshop_0321.service.ShoppingCartService;
 import com.portfolio.webshop_0321.service.ShoppingCartServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -19,6 +22,16 @@ public class ShoppingCartController {
     private ShoppingCartService shoppingCartService;
     @Autowired
     private CustomerService customerService;
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/shop")
+    public ModelAndView home() {
+        ModelAndView mav = new ModelAndView("shop");
+        List<Product> list = productService.findAll();
+        mav.addObject("products", list);
+        return  mav;
+    }
 
 /*    @GetMapping("/cart")
     public String showShoppingCart(Model model, @AuthenticationPrincipal Authentication authentication){
